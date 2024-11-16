a739ee9da6584fd57d3f40068c632bf703431e12
Dumbelfo <dumbelfo08@gmail.com>
2024-11-16 16:42:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tebas Template Creator
+
+Helps you easily create templates for the [Tebas project manager](https://github.com/Dumbelfo08/Tebas).
+
+Execute it in the folder with your files.
+It will ask you the name and if it uses git or not. Also, it will read the following files: **default.gitignore**, **readme.md** and **extensions.txt**.
+Inside of the **scripts** folder, it will read all **.tbscr** files and add them as scripts, taking as name the name of the file(without extension).
+Inside of the **resources** folder, it will read all files and add them as resources, no matter the etension, taking as name the name of the file(without extension).
---
+++ TebasTemplateCreator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using AshLib;
+
+class TebasTemplateCreator{
+	
+	static AshFile t;
+	
+	static void Main(string[] args){
+		Console.WriteLine("Welcome!");
+		Console.WriteLine();
+		
+		t = new AshFile();
+		string name = ask("Name of the template:");
+		t.SetCamp("name", name);
+		
+		t.SetCamp("git.defaultUse", askTF("Uses git?:"));
+		
+		loadFile("default.gitignore", "git.gitignore");
+		
+		loadFile("readme.md", "readme");
+		
+		loadFile("extensions.txt", "codeExtensions");
+		
+		string[] scripts = Directory.GetFiles(Directory.GetCurrentDirectory() + "/scripts", "*.tbscr", SearchOption.TopDirectoryOnly);
+		
+		foreach(string scr in scripts){
+			t.SetCamp("script." + Path.GetFileNameWithoutExtension(scr), File.ReadAllText("scripts/" + scr));
+			Console.WriteLine("Loaded script " + Path.GetFileNameWithoutExtension(scr));
+		}
+		
+		string[] resources = Directory.GetFiles(Directory.GetCurrentDirectory() + "/resources", "*.*", SearchOption.TopDirectoryOnly);
+		
+		foreach(string res in resources){
+			t.SetCamp("resources." + Path.GetFileNameWithoutExtension(res), File.ReadAllText("resources/" + res));
+			Console.WriteLine("Loaded resource " + res);
+		}
+		
+		t.Save(name + ".tbtem");
+		
+		Console.WriteLine("Bye!");
+	}
+	
+	static string ask(string q){
+		Console.Write(q + " ");
+		return Console.ReadLine();
+	}
+	
+	static bool askTF(string q){
+		string s;
+		do{
+			s = ask(q).ToLower();
+		}while(s != "true" || s != "false");
+		
+		return (s == "true" ? true : false);
+	}
+	
+	static void loadFile(string p, string o){
+		if(File.Exists(p)){
+			t.SetCamp(o, File.ReadAllText(p));
+			Console.WriteLine("Read file " + p);
+		}
+	}
+}
